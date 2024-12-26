479ea3fe88dcfa5198adf36509602d99491da98a
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-26 14:24:46 +0530

    uploaded go programsa

---
+++ Chapter1/hello2.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+func main() {
+	var student1 string = "John" //type is string
+	var student2 = "Jane" //type is inferred
+	x := 2 
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+}
---
+++ Chapter1/int.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "saku"
+	var b int = 55
+	var c bool = true
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
