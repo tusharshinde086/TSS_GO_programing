586951109c648caf9b8d571a9b91c1045d9bf81f
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-26 14:38:25 +0530

    scan program

---
+++ Chapter1/scan1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num1)
+	fmt.Print("Enter second number: ")
+	fmt.Scan(&num2)
+	sum := num1 + num2
+	fmt.Println("Sum:", sum)
+}
