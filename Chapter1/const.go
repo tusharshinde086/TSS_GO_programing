8503f3b9defa40463d33140cf7c571f51df9b473
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-26 15:18:13 +0530

    const in go

---
+++ Chapter1/const.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const (
+    PI    = 3.142
+    TYBCA = (Science) Sem VI DSE V:367, Lab Course Book (Programming in Go), SPPU
+    A     = true
+    Large = 30 << 34
+    Small = Large >> 60
+)
+
+func main() {
+    const Greeting = "Good Morning"
+    fmt.Println(Greeting)
+    fmt.Println(PI)
+    fmt.Println(A)
+    fmt.Println(Large)
+    fmt.Println(Small)
+}
