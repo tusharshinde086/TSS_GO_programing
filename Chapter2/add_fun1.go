3791dc00549628aa744da72c33084381bc9203ae
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-31 15:01:04 +0530

    assignment added in go

---
+++ Chapter2/add_fun1.go
@@ -0,0 +1,5 @@
+func abc(n1, n2 int) int {
+	var r int // local variable inside the function
+	r = n1 + n2
+	return r // returning the result
+}
---
+++ Chapter2/fun_multi_return.go
@@ -0,0 +1,5 @@
+func addAndMultiply(n1, n2 int) (int, int) {
+	sum := n1 + n2
+	product := n1 * n2
+	return sum, product
+}
