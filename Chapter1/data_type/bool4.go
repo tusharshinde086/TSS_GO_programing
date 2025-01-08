7b6cc78615c09743b9a81c23728b84939b70d1a8
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-08 14:58:04 +0530

    ass1 1. Data Type  5 programs uploaded

---
+++ Chapter1/data_type/bool4.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	isTrue := true
+	fmt.Println("Boolean:", isTrue)
+}
---
+++ Chapter1/data_type/int1.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 5
+	fmt.Println("Integer:", num)
+}
