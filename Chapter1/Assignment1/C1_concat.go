0f3b059e86e5a85fa2ea2787eb07d048e564186e
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-09 14:40:36 +0530

    assignment 1 set C concat

---
+++ Chapter1/Assignment1/C1_concat.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    var str1, str2 string
+	
+	fmt.Println("Enter aa strings   to concat :")
+
+      fmt.Println("Enter first string:")
+     fmt.Scan(&str1)
+     fmt.Println("Enter second string:")
+    fmt.Scan(&str2)
+
+    
+    result := &tr1 + str2)
+
+    fmt.Println("Concatenated string:", result)
+}
