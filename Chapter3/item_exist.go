37447777e46080ff276147989c2da8570b0879c0
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-24 15:38:08 +0530

    added item exists code

---
+++ Chapter3/item_exist.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	strArray := [5]string{"India", "Canada", "Japan", "Germany", "Italy"}
+	fmt.Println(itemExists(strArray, "India"))
+	fmt.Println(itemExists(strArray, "USA"))
+}
+func itemExists(arrayType interface{}, item interface{}) bool {
+	arr := reflect.ValueOf(arrayType)
+	if arr.Kind() != reflect.Array {
+		panic("Invalid data-type")
+	}
+	for i := 0; i < arr.Len(); i++ {
+		if arr.Index(i).Interface() == item {
+			return true
+		}
+	}
+	return false
+}
