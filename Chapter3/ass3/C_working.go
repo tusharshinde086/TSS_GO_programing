7c625ecd52200a37c581032489bd13f60c2471b3
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-24 16:04:30 +0530

    added demonstrate working of slices (like append, remove, copy, etc. code

---
+++ Chapter3/ass3/C_working.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Initial slice
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println("Initial slice:", slice)
+
+	// Append elements to the slice
+	slice = append(slice, 6, 7, 8)
+	fmt.Println("Slice after append:", slice)
+
+	// Remove an element at index 2 (remove 3)
+	slice = append(slice[:2], slice[3:]...)
+	fmt.Println("Slice after removing element at index 2:", slice)
+
+	// Copy slice
+	copiedSlice := make([]int, len(slice))
+	copy(copiedSlice, slice)
+	fmt.Println("Copied slice:", copiedSlice)
+}
