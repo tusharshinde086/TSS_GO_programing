b8d4cf12a85bb68300b73234a1870feea0d61482
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-24 15:15:01 +0530

    added slice code

---
+++ Chapter3/multi_d.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var theArray [3]string
+	theArray[0] = "India"
+	theArray[1] = "Canada"
+	theArray[2] = "USA"
+	fmt.Println(theArray[0])
+	fmt.Println(theArray[1])
+	fmt.Println(theArray[2])
+}
