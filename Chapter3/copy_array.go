c2c1e23f139ac87244ee560f1aed868608c294ac
Tushar Shinde <tusharshindets086@gmail.com>
2025-01-24 15:36:53 +0530

    added copy of array code

---
+++ Chapter3/copy_array.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	strArray1 := [3]string{"India", "Australia", "Germany"}
+
+	strArray2 := strArray1  // data is passed by value
+	strArray3 := &strArray1 // data is passed by reference
+
+	fmt.Printf("strArray1: %v\n", strArray1)
+	fmt.Printf("strArray2: %v\n", strArray2)
+
+	strArray1[0] = "Canada"
+
+	fmt.Printf("strArray1: %v\n", strArray1)
+	fmt.Printf("strArray2: %v\n", strArray2)
+	fmt.Printf("*strArray3: %v\n", *strArray3)
+}
