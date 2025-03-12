4c13e63cb762a822e5e4aadf532c69d607e86672
Tushar Shinde <tusharshindets086@gmail.com>
2025-03-12 21:42:32 +0530

    if

---
+++ Chapter1/decision/if2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 10
+
+	if num > 5 {
+		fmt.Println("Number is greater than 5")
+	}
+
+	if num%2 == 0 {
+		fmt.Println("Number is even")
+	} else {
+		fmt.Println("Number is odd")
+	}
+
+	if num := 7; num < 10 { // Short statement in if
+		fmt.Println("Number is less than 10")
+	}
+}
